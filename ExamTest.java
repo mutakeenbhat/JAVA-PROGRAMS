a3ae10eba02ba9a195673a0e532307b0440b459e
mutakeenbhat <90750027+mutakeenbhat@users.noreply.github.com>
2022-09-14 19:59:57 +0530

    Add files via upload

---
+++ ExamTest.java
@@ -0,0 +1,8 @@
+public class ExamTest {
+    
+
+public static void main (String args[]){
+    System.out.println(10 + 20 + "IUST");
+    System.out.println("IUST" + 10 + 20);
+    System.out.println("IUST" + 10 * 20);
+}}
