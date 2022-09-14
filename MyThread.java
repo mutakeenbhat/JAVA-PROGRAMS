89a543af7f386b2f0c8d4081c83faee105040f51
mutakeenbhat <90750027+mutakeenbhat@users.noreply.github.com>
2022-09-14 20:07:23 +0530

    Add files via upload

---
+++ MyThread.java
@@ -0,0 +1,11 @@
+
+public class MyThread extends Thread {
+    public static void main(String[] args) {
+      MyThread thread = new MyThread();
+      thread.start();
+      System.out.println("This code is outside of the thread");
+    }
+    public void run() {
+      System.out.println("This code is running in a thread");
+    }
+  }
