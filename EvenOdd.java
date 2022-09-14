ea956d3b2f25de3f89f293cb45b4f2f577ff0a6e
mutakeenbhat <90750027+mutakeenbhat@users.noreply.github.com>
2022-09-14 19:59:11 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,18 @@
+/* program to the no is even or odd */
+import java.util.*;
+public class EvenOdd {
+    public static void main(String [] args)
+    {
+        int x;
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Enter the number :");
+            x= sc.nextInt();
+       if(x % 2==0)
+      System.out.println(" THe entered no is even :");
+      else
+       System.out.println(" The entered no is odd :");
+
+
+    }
+    
+}
