b257879117e4f75ea9beab0405d8831fdc811f28
mutakeenbhat <90750027+mutakeenbhat@users.noreply.github.com>
2022-09-14 19:56:20 +0530

    Add files via upload

---
+++ ArrayEle.java
@@ -0,0 +1,10 @@
+class ArrayEle{
+    public static void main(String [] args){
+        int []arr = new int [] {1,2 ,3,4,5};
+        System.out.println(" Elements of an array are :");
+        for( int i = 0; i <arr.length; i++){
+         System.out.println( arr[i]+ " ");
+         
+        }
+    }
+}
