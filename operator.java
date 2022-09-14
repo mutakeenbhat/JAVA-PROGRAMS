1e16315e35eaf01dc32812272adc2b4594fb4298
mutakeenbhat <90750027+mutakeenbhat@users.noreply.github.com>
2022-09-14 20:08:20 +0530

    Add files via upload

---
+++ operator.java
@@ -0,0 +1,27 @@
+/*program on operators */
+import java.util.*;
+
+public class operator {
+    public static void main(String[] args){
+        int x,y;
+        Scanner sc = new Scanner(System.in);
+       System.out.println("Enter the value of x :");
+       x = sc.nextInt();
+       System.out.println("Enter the value of y :");
+       y = sc.nextInt();
+if(x<y)
+System.out.println("x is less than y");
+x=x*2;
+if(x==y)
+System.out.println("x is equal to y");
+x=x*2;
+if(x>y)
+System.out.println("x is greater than y");
+
+   if(x==y)
+   System.out.println("u won't see it") ;
+   
+
+    }
+    
+}
